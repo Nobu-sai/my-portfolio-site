78eaa57eea967c68382abd7ecfaf8cad3137226f
snz0901@gmail.com <ngbt0409>
2020-10-02 17:36:39 +0900

    ADD: icons for skill

--- portfolio-site-react/src/components/Skill.js
+++ portfolio-site-react/src/components/Skill.js
@@ -1,41 +1,74 @@
 import React from 'react'
 import '../style/section.css';
 import '../style/skill.css';
+import htmlIcon from '../assets/skill-icon/html5.svg';
+import cssIcon from '../assets/skill-icon/css3.svg';
+import bootstrapIcon from '../assets/skill-icon/bootstrap.svg';
+import jsIcon from '../assets/skill-icon/js.svg';
+import reactjsIcon from '../assets/skill-icon/reactjs.svg';
+import nodejsIcon from '../assets/skill-icon/nodejs.svg';
+import expressjsIcon from '../assets/skill-icon/expressjs.svg';
+import firebaseIcon from '../assets/skill-icon/firebase.svg';
+import mongodbIcon from '../assets/skill-icon/mongodb.svg';
+import figmaIcon from '../assets/skill-icon/figma.svg';
+
+
 
 function Skill() {
   return (
 
     <div className="section" id="skill">
       <div className="section__heading_container">
-        <h1 className="section__heading">Skill</h1>
+        <div className="section__heading">Skill</div>
       </div>
       
-      <div className="section_contents skill_contents">        
-          {/* <table className="skill_table">
-            <tr><td colspan="4" className="skill_category">Front End</td></tr>            
-            <tr><td>HTML</td><td>CSS</td><td>JS</td><td>React.js</td></tr>
-            <tr><td colspan="4" className="skill_category">Back End</td></tr>              
-            <tr><td>Node.js</td><td>Firebase</td><td>MongoDB</td></tr>
-                
-          </table>  */}
-        <div className="skill_list">
-            <div className="skill_category">Front End</div>
-            <div className="skill_item">HTML</div>
-            <div className="skill_item">CSS</div>
-            <div className="skill_item">JS</div>
-            <div className="skill_item">React.js</div>
-            <div className="skill_category">Back End</div>
-            <div className="skill_item">Node.js</div>
-            <div className="skill_item">Firebase</div>
-            <div className="skill_item">MongoDB</div>            
-        </div> 
-        
+      <div className="section_contents skill_contents">                        
+        <div className="skill_item">
+          <div class="skill__item_name">HTML</div>
+          <img class="skill__item_icon" src={htmlIcon}></img>
+        </div>
+        <div className="skill_item">
+          <div class="skill__item_name">CSS</div>
+          <img class="skill__item_icon" src={cssIcon}></img>
+        </div>
+        <div className="skill_item">
+          <div class="skill__item_name">Bootstrap</div>
+          <img class="skill__item_icon" src={bootstrapIcon}></img>
+        </div>
+        <div className="skill_item">
+          <div class="skill__item_name">JS</div>
+          <img class="skill__item_icon" src={jsIcon}></img>
+        </div>
+        <div className="skill_item">
+          <div class="skill__item_name">React.js</div>
+          <img class="skill__item_icon" src={reactjsIcon}></img>
+        </div>            
+        <div className="skill_item">
+          <div class="skill__item_name">Node.js</div>
+          <img class="skill__item_icon" src={nodejsIcon}></img>
+        </div>
+        <div className="skill_item">
+          <div class="skill__item_name">Express.js</div>
+          <img class="skill__item_icon" src={expressjsIcon}></img>
+        </div>
+        <div className="skill_item">
+          <div class="skill__item_name">Firebase</div>
+          <img class="skill__item_icon" src={firebaseIcon}></img>
+        </div>
+        <div className="skill_item">
+          <div class="skill__item_name">MongoDB</div>
+          <img class="skill__item_icon" src={mongodbIcon}></img>
+        </div>     
+        <div className="skill_item">
+          <div class="skill__item_name">Figma</div>
+          <div className="coming_soon">* Coming Soon</div>
+          <img class="skill__item_icon" src={figmaIcon}></img>
+        </div>               
       </div>
 
-      <p className="image_source" >
-        <a href="https://images.unsplash.com/photo-1475873326779-99eac8da25b0?ixlib=rb-1.2.1&auto=format&fit=crop&w=933&q=80">Image Source</a>
-      </p>         
+
     </div>
+
 
 
   )
