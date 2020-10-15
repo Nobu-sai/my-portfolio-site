0bbe24db5f49543f927677bd09fedd30c7e58213
snz0901@gmail.com <ngbt0409>
2020-10-15 15:54:50 +0900

    CHNG: Build UI List from Firebase portoflio data

--- portfolio-site-react/src/components/PageContents/Portfolio/Portfolio.js
+++ portfolio-site-react/src/components/PageContents/Portfolio/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Heading from '../../Heading/Heading.js'
 import PortfolioDesc from './PortfolioDesc.js';
 import PortfolioList from './PortfolioList.js'
@@ -6,51 +6,72 @@ import '../Section.css';
 import './Portfolio.css';
 import { db } from '../../../firebase.js';
 
-function Portfolio() {
+class Portfolio extends React.Component {
 
-  const [portfolioToShow, setPortfolioToShow] = useState(null)
-  
-  let isInitial = true;
+  constructor(props) {
+    super(props)
+    this.state = {portfolioData: []}
+    this.state = {portfolioToShow: null}    
+    this.isInitial = true;
+  }
 
-  let portfolioData = [];  
+  // componentDidMount() {
+  //   this.portfolioDataHandler()    
+  // }
 
-  useEffect(()=>{
-    portfolioDataHandler()    
-  }, [])
-
-  const portfolioDataHandler = () => {
-    db.collection('portfolio-data').onSnapshot( (snapshot)=>{
-      // console.log(snapshot.docs)
-        snapshot.forEach( (doc)=> {              
-        portfolioData.push(doc.data())    
-          // setPortfolioData([...portfolioData, doc.data()])
-        })
-        console.log(portfolioData)    
-        portfolioListHandler()            
-    })  
-    console.log(portfolioData)     
-    
+  componentWillUnmount() {
+    this.portfolioDataHandler()    
   }
 
 
-// Set  portfolio to show 
-// => If it is inital state or before the user click an item of portfolio, then set the first item of  portfolioData Array  as the  portoflio to show .
-// => If there is an List Item clicked, it is set as the  portfolio to show .
-// * Then, it might be better to call this Funciton as  portfolioToShowHander() , because it is unnecessarily handled by the List. 
- const portfolioListHandler = (clickedListItem) => {
-    setPortfolioToShow("")
-    if(isInitial) {
-      setPortfolioToShow(portfolioData[0])
-      isInitial = false;        
-      console.log(portfolioData)         
+  portfolioDataHandler = () => {
+    let portfolioDataList = []
+
+    db.collection('portfolio-data').onSnapshot((snapshot)=>{
+      // console.log(snapshot.docs)      
+        snapshot.forEach((doc)=> {  
+          portfolioDataList.push(doc.data())    
+          // this.setState({portfolioData: portfolioDataList})        
+        })
+        console.log("portfolioDataList: ", portfolioDataList)     
+        console.log("portfolioDataList Array without Text (Underneath) * To See [] in Line 33 VS [{...},{...}] in Line 42 ")    
+        console.log(" * To See [] in Line 35 VS [{...},{...}] in Line 44 ")    
+        console.log(portfolioDataList)        
+        console.log("portfolioData State: ", this.state.portfolioData) 
+        this.setState({portfolioData: portfolioDataList})           
+        console.log("portfolioData State: ", this.state.portfolioData)   
+        
+    })      
+    
+    console.log("portfolioDataList: ", portfolioDataList)     
+    console.log("portfolioDataList Array without Text (Underneath)")    
+    console.log(" * To See [] in Line 35 VS [{...},{...}] in Line 44 ")    
+    console.log(portfolioDataList)       
+    console.log("portfolioDataList in JSON.stringify(): ",  JSON.stringify(portfolioDataList))
+    console.log("portfolioData State: ", this.state.portfolioData)     
+    this.setState({portfolioData: portfolioDataList})
+    console.log("portfolioData State: ", this.state.portfolioData)     
+      
+
+  }
+
+
+  // Set  portfolio to show 
+  // => If it is inital state or before the user click an item of portfolio, then set the first item of  portfolioData Array  as the  portoflio to show .
+  // => If there is an List Item clicked, it is set as the  portfolio to show .
+  // * Then, it might be better to call this Funciton as  portfolioToShowHander() , because it is unnecessarily handled by the List. 
+  portfolioListHandler = (clickedListItem) => {    
+    if(this.isInitial) {
+      this.portfolioToShow = this.state.portfolioData[0]
+      this.isInitial = false;        
+      console.log(this.state.portfolioData)         
     } else {
-      setPortfolioToShow(portfolioData[clickedListItem]);   
+      this.portfolioToShow = this.state.portfolioData[clickedListItem];   
       
     } 
-  
   }  
 
-  // const addData = () => {      
+  // addData = () => {      
   //     db.collection('portfolio-data').add(
   //       {
   //         name: `${portfolio[1].name}`,
@@ -63,33 +84,50 @@ function Portfolio() {
 
   // }
 
-  return (
-    <div className="section" id="portfolio">
-      <Heading  content="Portfolio" />
+  consoleLogState = () => {
+    this.state.portfolioData && console.log("portfolioData State", this.state.portfolioData)
+  }
   
-      <div className="section_contents portfolio_contents">  
 
-      {/* <button onClick={e => addData()}>Submit Portfolio</button> */}     
-      
-      {        
-        <PortfolioList 
-          portfolioData={portfolioData} 
-          portfolioListHandler={portfolioListHandler}
-        />        
-      } 
+  render() {
+    return (
+
+      <div className="section" id="portfolio">
+        <Heading  content="Portfolio" />
     
-
-      {
-        portfolioToShow &&
-        <PortfolioDesc 
-          portfolioToShow={portfolioToShow}
-        />  
-      }
-      
-      </div>
-
-    </div>  
-  )
+        <div className="section_contents portfolio_contents">  
+  
+        {/* <button onClick={e => addData()}>Submit Portfolio</button> */}     
+        {this.state.portfolioData && console.log("portfolioData State, line 90", this.state.portfolioData)}
+        {console.log("portfolioData State, line 91", this.state.portfolioData)}
+        
+        {
+          this.state.portfolioData &&
+          <PortfolioList 
+            portfolioData={this.state.portfolioData} 
+            portfolioListHandler={this.portfolioListHandler}
+          />        
+        
+        }
+  
+        {
+          // this.state.portfolioData &&
+          // console.log(this.portfolioToShow)
+          // <PortfolioDesc 
+          //   portfolioToShow={this.portfolioListHandler}
+          // />  
+        }  
+        
+        
+          
+        
+        
+        </div>
+  
+      </div>  
+    )
+  }
 }
 
-export default Portfolio
+
+export default Portfolio;
