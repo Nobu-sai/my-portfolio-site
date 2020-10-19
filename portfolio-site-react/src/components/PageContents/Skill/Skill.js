60de1e26307a6007e47c69d767bc3315b2f76a02
snz0901@gmail.com <ngbt0409>
2020-10-19 12:27:15 +0900

    CHNG: use  require() to import files

--- portfolio-site-react/src/components/PageContents/Skill/Skill.js
+++ portfolio-site-react/src/components/PageContents/Skill/Skill.js
@@ -28,47 +28,46 @@ function Skill() {
       <div className="section_contents skill_contents">                        
         <div className="skill_item">
           <div className="skill__item_name">HTML</div>
-          <img className="skill__item_icon" src={htmlIcon} alt="HTML icon"></img>
+          <img className="skill__item_icon" src={require('../../../assets/skill-icon/html5.svg')} alt="HTML icon"></img>
         </div>
         <div className="skill_item">
           <div className="skill__item_name">CSS</div>
-          <img className="skill__item_icon" src={cssIcon} alt="CSS icon"></img>
+          <img className="skill__item_icon" src={require('../../../assets/skill-icon/css3.svg')} alt="CSS icon"></img>
         </div>
         <div className="skill_item">
           <div className="skill__item_name">Bootstrap</div>
-          <img className="skill__item_icon" src={bootstrapIcon}alt="Bootstrape icon"></img>
+          <img className="skill__item_icon" src={require('../../../assets/skill-icon/bootstrap.svg')} alt="Bootstrape icon"></img>
         </div>
         <div className="skill_item">
           <div className="skill__item_name">JavaScript</div>
-          <img className="skill__item_icon" src={jsIcon} alt="JavaScript icon"></img>
+          <img className="skill__item_icon" src={require('../../../assets/skill-icon/js.svg')} alt="JavaScript icon"></img>
         </div>
         <div className="skill_item">
           <div className="skill__item_name">React.js</div>
-          <img className="skill__item_icon" src={reactjsIcon}
+          <img className="skill__item_icon" src={require('../../../assets/skill-icon/reactjs.svg')}
           alt="React.js icon"></img>
         </div>            
         <div className="skill_item">
           <div className="skill__item_name">Node.js</div>
-          <img className="skill__item_icon" src={nodejsIcon}
+          <img className="skill__item_icon" src={require('../../../assets/skill-icon/nodejs.svg')}
           alt="Node.js icon"></img>
         </div>
         <div className="skill_item">
           <div className="skill__item_name">Express.js</div>
-          <img className="skill__item_icon" src={expressjsIcon}alt="Express.js icon"></img>
+          <img className="skill__item_icon" src={require('../../../assets/skill-icon/expressjs.svg')}alt="Express.js icon"></img>
         </div>
         <div className="skill_item">
           <div className="skill__item_name">Firebase</div>
-          <img className="skill__item_icon" src={firebaseIcon}alt="Firebase icon"></img>
+          <img className="skill__item_icon" src={require('../../../assets/skill-icon/firebase.svg')}alt="Firebase icon"></img>
         </div>
         <div className="skill_item">
           <div className="skill__item_name">MongoDB</div>
-          <img className="skill__item_icon" src={mongodbIcon}
+          <img className="skill__item_icon" src={require('../../../assets/skill-icon/mongodb.svg')}
           alt="MongoDB icon"></img>
         </div>     
         <div className="skill_item">
           <div className="skill__item_name">Figma</div>
-          {/* <div className="coming_soon">* Coming Soon</div> */}
-          <img className="skill__item_icon" src={figmaIcon}
+          <img className="skill__item_icon" src={require('../../../assets/skill-icon/figma.svg')}
           alt="Figma icon"></img>
         </div>               
       </div>
