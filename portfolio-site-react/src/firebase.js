a64070710f4f626e285921d2af24d903d317cc67
snz0901@gmail.com <ngbt0409>
2020-10-22 15:49:27 +0900

    CHNG: firestore

--- portfolio-site-react/src/firebase.js
+++ portfolio-site-react/src/firebase.js
@@ -5,13 +5,6 @@ import firebase from 'firebase';
 const firebaseApp = firebase.initializeApp(
   {  
   // CONFIG GOES HERE
-  apiKey: "AIzaSyDDMwGF54bjByrjth3iaw2pA6Sbg4VabUI",
-  authDomain: "portfolio-site-ad71f.firebaseapp.com",
-  databaseURL: "https://portfolio-site-ad71f.firebaseio.com",
-  projectId: "portfolio-site-ad71f",
-  storageBucket: "portfolio-site-ad71f.appspot.com",
-  messagingSenderId: "1066914805547",
-  appId: "1:1066914805547:web:8f855bea6d5aae606f776e"
   }  
 )
 const db = firebaseApp.firestore()
