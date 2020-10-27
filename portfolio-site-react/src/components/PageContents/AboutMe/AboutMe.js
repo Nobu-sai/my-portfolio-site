c8739b207d3c2c2db674eded8a8835ca6167ff2f
snz0901@gmail.com <ngbt0409>
2020-10-27 16:22:21 +0900

    CHNG: yellow markers

--- portfolio-site-react/src/components/PageContents/AboutMe/AboutMe.js
+++ portfolio-site-react/src/components/PageContents/AboutMe/AboutMe.js
@@ -14,15 +14,15 @@ function AboutMe() {
         
         <div className="section_contents about_me_contents">
           <div className="upper_contents">            
-            <div className="intro_heading"> Coding is <br/>my <br/>partner.<br/>I want to be<br/>your <br/>partner.</div>     
+            <div className="intro_heading"> Coding is <br/>my <br/><span class="yellow-marker">partner</span>.<br/>I want to be<br/>your <br/><span class="yellow-marker">partner</span>.</div>     
             <div className="profile_image"></div>   
           </div>
                 
 
          <div className="intro_message">
-         I love coding, because I love creating. <br/>
+         I love coding, because I love <span class="yellow-marker">creating</span>. <br/>
          At the end, <br/>coding is a way for creating things, <br/> and it is my great partner.<br/>
-          I want to be your partner, <br/> who makes your imagination into the reality.
+          I want to be your partner, <br/> who makes<span class="yellow-marker"> your imagination into the reality</span>.
          </div>    
                                 
         
