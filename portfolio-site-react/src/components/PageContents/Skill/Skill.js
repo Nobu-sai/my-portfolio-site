6fdcb40b7b1c19c47fa55725916244a862b3241b
snz0901@gmail.com <ngbt0409>
2020-10-27 11:52:18 +0900

    CHNG: optimize skill items (component, iteration)

--- portfolio-site-react/src/components/PageContents/Skill/Skill.js
+++ portfolio-site-react/src/components/PageContents/Skill/Skill.js
@@ -19,7 +19,6 @@ function Skill() {
     db.collection('skill').onSnapshot((snapshot)=>{
       console.log("...When: React is going to read  setSkills Method, so it Will Render the return()")
       setSkills(snapshot.docs.map((doc)=> doc.data() ))
-      // snapshot.forEach((doc)=>setState([...skills, doc.data()]))
       })
   }
 
@@ -29,184 +28,13 @@ function Skill() {
     <div className="section" id="skill">
       <Heading  content="Skill" />
       <button onClick={Skill}>Add skill</button>
-      <div className="section_contents skill_contents">     
-      {skills && console.log(skills)}
-      {skills && console.log("* What: skills State")}
-      {skills && console.log("* Where: Inside return()")}
-      {skills.length > 1 && console.log(skills[0].logo)}
-      {skills.length > 1  && console.log("* What: skills[0].logo")}
-      {skills.length > 1  && console.log("* Where: INSIDE return()")}
-      {skills.length > 1  && console.log("* When: BEFORE map()")}
+      <div className="section_contents skill_contents">        
       {skills.length > 1 && skills.map((skill)=>(
 
-        <SkillItem skillName={skill.name} />
-        // <div className="skill_item">
-        // <div className="skill__item_name">{skill.name}</div>
-        //   <div className="skill__item_icon"             
-        //     style={{
-        //       // background: `url(${require("../../../assets/skill-icon/javascript.svg")})`,
-        //       // Copied Path from Firestore.
-        //         // => CAN be accessed. 
-        //       // background: `url(${require("../../../assets/skill-icon/javascript.svg")})`,
-        //       // Copied Path from logged path in Chrome/Console.
-        //         // => CAN be accessed.   
-        //       background: `url(${require(skill.logo)})`,
-        //       backgroundPosition: "center",              
-        //       backgroundRepeat: "no-repeat",
-        //       backgroundSize: "contain",
-        //     }}>
-        //   </div>                  
-        // </div>          
-
+        <SkillItem skillName={skill.name} />          
                
       ))} 
-      
-        
-        {/* <div className="skill_item">
-          <div className="skill__item_name">HTML</div>
-          <div className="skill__item_icon"             
-            style={{
-              background: `url(${require('../../../assets/skill-icon/html5.svg')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}>
-          </div>              
-        </div> */}
-        {/* <div className="skill_item">
-          <div className="skill__item_name">CSS</div>
-          <div className="skill__item_icon"             
-            style={{
-              background: `url(${require('../../../assets/skill-icon/css3.svg')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}>              
-            </div>
-        </div>     */}
-        {/* <div className="skill_item">
-          <div className="skill__item_name">Bootstrap</div>
-          <div className="skill__item_icon"             
-            style={{
-              background: `url(${require('../../../assets/skill-icon/bootstrap.svg')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}>
-          </div>      
-        </div> */}
-        {/* <div className="skill_item">
-          <div className="skill__item_name">JavaScript</div>
-          <div className="skill__item_icon"             
-            style={{
-              background: `url(${require('../../../assets/skill-icon/javascript.svg')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}>
-          </div>     
-        </div>   */}
-        {/* <div className="skill_item">
-          <div className="skill__item_name">React.js</div>
-          <div className="skill__item_icon"             
-          style={{
-              background: `url(${require('../../../assets/skill-icon/reactjs.svg')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}>              
-          </div>
-        </div>                       */}
-        {/* <div className="skill_item">
-          <div className="skill__item_name">Node.js</div>
-          <div className="skill__item_icon"             
-            style={{
-              background: `url(${require('../../../assets/skill-icon/nodejs.svg')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}></div>
-        </div> */}
-        {/* <div className="skill_item">
-          <div className="skill__item_name">Express.js</div>
-          <div className="skill__item_icon"             
-            style={{
-              background: `url(${require('../../../assets/skill-icon/expressjs.svg')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}>
-           </div>   
-        </div> */}
-        {/* <div className="skill_item">
-          <div className="skill__item_name">Firebase</div>
-          <div className="skill__item_icon"             
-            style={{
-              background: `url(${require('../../../assets/skill-icon/firebase.svg')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}>              
-          </div>
-        </div>     */}
-        {/* <div className="skill_item">
-          <div className="skill__item_name">MongoDB</div>
-          <div className="skill__item_icon"             
-            style={{
-              background: `url(${require('../../../assets/skill-icon/mongodb.svg')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}>
-          </div>    
-        </div>      */}
-        {/* <div className="skill_item">
-          <div className="skill__item_name">Figma</div>
-          <div className="skill__item_icon"             
-            style={{
-              background: `url(${require('../../../assets/skill-icon/figma.svg')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}>
-           </div>   
-        </div>       */}
-
-        {/* <div className="skill_item">
-          <div className="skill__item_name">Postman</div>
-          <div className="skill__item_icon" 
-            style={{
-              background: `url(${require('../../../assets/skill-icon/postman.png')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}>              
-          </div>
-        </div>       */}
-
-        {/* <div className="skill_item">
-          <div className="skill__item_name">Storybook.js</div>
-          <div className="skill__item_icon" 
-            style={{              
-              background: `url(${require('../../../assets/skill-icon/storybook.svg')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}>
-          </div>                
-        </div>   */}
-        {/* <div className="skill_item">
-          <div className="skill__item_name">Flexbox</div>
-          <div 
-            className="skill__item_icon flexbox-icon" 
-            style={{              
-              background: `url(${require('../../../assets/skill-icon/flexbox.jpg')})`,
-              backgroundPosition: "center",              
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-            }}>
-          </div>                
-        </div>   */}
+               
 
       </div>
     </div>
--- portfolio-site-react/src/components/PageContents/Skill/SkillItem.js
+++ portfolio-site-react/src/components/PageContents/Skill/SkillItem.js
@@ -5,16 +5,7 @@ import SkillLogo from './SkillLogo.js'
 
 function SkillItem({skillName}) {
   
-  const [skillLogo, setSkillLogo] = useState()
-  console.log(SkillLogo)
-  console.log(SkillLogo.JavaSctipt)
-  console.log("* What: SkillLogo.JavaSctipt")
-  console.log(SkillLogo[skillName])
-  console.log("* What: SkillLogo[skillName]")
-  console.log(skillName)
-  console.log("* What: skillName")
-  console.log(skillLogo)
-  console.log("* What: skillLogo")
+  const [skillLogo, setSkillLogo] = useState()  
 
   useEffect(()=>{
     setSkillLogo(SkillLogo[skillName])
@@ -24,21 +15,17 @@ function SkillItem({skillName}) {
   return (
     <div className="skill_item">      
     {skillLogo &&
-    <>
+      <>
           <div className="skill__item_name">{skillName}</div>
           <div className="skill__item_icon"             
-            style={{
-              // background: `url(${require(skillData.logo)})`,
-              // background: `url(${require("../../../assets/skill-icon/javascript.svg")})`,
-              // Copied Path from Firestore.
-                // => CAN be accessed.             
+            style={{            
               backgroundImage: `url(${skillLogo})`,             
               backgroundPosition: "center",              
               backgroundRepeat: "no-repeat",
               backgroundSize: "contain",
             }}>
           </div>  
-    </>                      
+      </>                      
     }     
     
     </div>          
