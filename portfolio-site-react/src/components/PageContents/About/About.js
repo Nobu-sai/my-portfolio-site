d7faf97a1da80b173a06691b4ec0ca7872bd26af
snz0901@gmail.com <ngbt0409>
2020-10-31 17:21:06 +0900

    CHNG: marker design and <br/>

--- portfolio-site-react/src/components/PageContents/About/About.js
+++ portfolio-site-react/src/components/PageContents/About/About.js
@@ -20,9 +20,9 @@ function About() {
                 
 
          <div className="intro_message">
-         I love coding, because I love <span class="green_marker">creating</span>. <br/>
-         At the end, <br/>coding is <span class="yellow_marker">a helpful way</span> for creating things, <br/> and it is my great partner.<br/>
-          I want to be your partner, <br/> who <span class="yellow_marker">stays with you</span> to <br/>make<span class="green_marker"> your imagination into the reality</span>.
+         I love <span class="yellow_marker">coding</span>, because I love <span class="green_marker">creating</span>. <br/>
+         At the end, <br/>coding is <span class="yellow_marker">a helpful way</span><br/>for <span class="green_marker">creating what I want</span>, <br/> and it is my great partner.<br/>
+          I want to be your partner,<br/><span class="yellow_marker">who stays with you</span> <br/>to <span class="green_marker">make your imagination into the reality</span>.
          </div>    
                                 
         
