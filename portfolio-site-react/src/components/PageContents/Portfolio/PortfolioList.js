c60afd21a3d767801ce9aa215fd009c5f418744b
snz0901@gmail.com <ngbt0409>
2020-11-07 15:28:57 +0900

    TRY: SkillLogo.js for Portfolio List

--- portfolio-site-react/src/components/PageContents/Portfolio/PortfolioList.js
+++ portfolio-site-react/src/components/PageContents/Portfolio/PortfolioList.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import PortfolioSkill from './PortfolioSkill.js'
+
 import './Portfolio.css'
 import { v4 as uuidv4 } from 'uuid';
 
-function PortfolioList({ portfolioData, portfolioListHandler }) {
+function PortfolioList({ portfolioData, portfolioListHandler }) {  
+  console.log(portfolioData[0].skill_list)
+  portfolioData.map((portfolio)=>{console.log(portfolio.skill_list)})
 
    return (
     <div className="PortfolioList">
@@ -10,7 +14,7 @@ function PortfolioList({ portfolioData, portfolioListHandler }) {
       <div className="portfolio_list">            
         <div className="portfolio_list__background">                
           {
-            portfolioData.map((portfolio)=> (                 
+            portfolioData.map((portfolio)=> (                             
               <div 
               className="portfolio_list__item"             
               onClick={e => portfolioListHandler(portfolio.name)}
@@ -23,7 +27,9 @@ function PortfolioList({ portfolioData, portfolioListHandler }) {
                   <div className="portfolio_data__item">
                     <div 
                       className="portfolio_data__title"> 
-                      Skill<br/> <span className="portfolio_skill__name yellow_marker"> {portfolio.skill}.</span>
+                      Skill<br/> 
+                      {/* <span className="portfolio_skill__name yellow_marker"> {portfolio.skill}.</span> */}
+                        <PortfolioSkill className="portfolio_skill__name" skillList={portfolio} />
                     </div> 
                   </div>
 
---
+++ portfolio-site-react/src/components/PageContents/Portfolio/PortfolioSkill.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PortfolioSkill({portfolio}) {
+  console.log(portfolio)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PortfolioSkill;
