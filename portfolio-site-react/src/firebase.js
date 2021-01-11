0bccd0cbf0ed649efd33f55008993475f4e09696
snz0901@gmail.com <ngbt0409>
2021-01-11 19:09:31 +0900

    test-2-case-2

--- portfolio-site-react/src/firebase.js
+++ portfolio-site-react/src/firebase.js
@@ -11,17 +11,18 @@ const firebaseDefaultProjectConfig  = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID 
 }
 
-const firebaseCommitmentRecordProjectConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_COMMITMENT_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_COMMITMENT_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_COMMITMENT_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_COMMITMENT_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_COMMITMENT_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_COMMITMENT_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_COMMITMENT_APP_ID 
-}
+// const firebaseCommitmentRecordProjectConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_COMMITMENT_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_COMMITMENT_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_FIREBASE_COMMITMENT_DATABASE_URL,
+//   projectId: process.env.REACT_APP_FIREBASE_COMMITMENT_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_COMMITMENT_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_COMMITMENT_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_COMMITMENT_APP_ID 
+// }
 
 // Access to Firebase Services.
+// // portfolio-site Project
 const defaultProject = firebase.initializeApp(firebaseDefaultProjectConfig)
 const defaultDB = defaultProject.firestore()
 // const defaultAuth = defaultProject.auth();
@@ -29,16 +30,17 @@ const defaultDB = defaultProject.firestore()
 
 export { defaultDB };
 
-const commitmentRecordProject = firebase.initializeApp(firebaseCommitmentRecordProjectConfig, "other")
-const commitmentRecordDB = commitmentRecordProject.firestore()
-// const defaultAuth = defaultProject.auth();
-// const storage = firebase.storage();
+// // commitment-record Project
+// const commitmentRecordProject = firebase.initializeApp(firebaseCommitmentRecordProjectConfig, "other")
+// const commitmentRecordDB = commitmentRecordProject.firestore()
+// // const defaultAuth = defaultProject.auth();
+// // const storage = firebase.storage();
 
 
-// Test the name
-console.log(defaultProject.name)
-console.log(commitmentRecordProject.name)
+// // Test the name
+// console.log(defaultProject.name)
+// console.log(commitmentRecordProject.name)
 
 // Exporting
 
-export { commitmentRecordDB };
+// export { commitmentRecordDB };
