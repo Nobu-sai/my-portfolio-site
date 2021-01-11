2a620d7030d05ed7063a21bb4f71a2c38754abf6
snz0901@gmail.com <ngbt0409>
2021-01-11 19:13:15 +0900

    test-2-case-2

--- portfolio-site-react/src/firebase.js
+++ portfolio-site-react/src/firebase.js
@@ -11,16 +11,6 @@ const firebaseDefaultProjectConfig  = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID 
 }
 
-// const firebaseCommitmentRecordProjectConfig = {
-//   apiKey: process.env.REACT_APP_FIREBASE_COMMITMENT_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_COMMITMENT_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_FIREBASE_COMMITMENT_DATABASE_URL,
-//   projectId: process.env.REACT_APP_FIREBASE_COMMITMENT_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_COMMITMENT_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_COMMITMENT_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_COMMITMENT_APP_ID 
-// }
-
 // Access to Firebase Services.
 // // portfolio-site Project
 const defaultProject = firebase.initializeApp(firebaseDefaultProjectConfig)
@@ -29,18 +19,3 @@ const defaultDB = defaultProject.firestore()
 // const storage = firebase.storage();
 
 export { defaultDB };
-
-// // commitment-record Project
-// const commitmentRecordProject = firebase.initializeApp(firebaseCommitmentRecordProjectConfig, "other")
-// const commitmentRecordDB = commitmentRecordProject.firestore()
-// // const defaultAuth = defaultProject.auth();
-// // const storage = firebase.storage();
-
-
-// // Test the name
-// console.log(defaultProject.name)
-// console.log(commitmentRecordProject.name)
-
-// Exporting
-
-// export { commitmentRecordDB };
