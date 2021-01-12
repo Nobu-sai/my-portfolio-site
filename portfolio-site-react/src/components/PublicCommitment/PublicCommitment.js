13af2f3aef5df023793c639ecb8f5b9828c8322c
snz0901@gmail.com <ngbt0409>
2021-01-12 19:05:54 +0900

    CHNG: Public Commitment: clock like animation

--- portfolio-site-react/src/components/PublicCommitment/PublicCommitment.js
+++ portfolio-site-react/src/components/PublicCommitment/PublicCommitment.js
@@ -9,6 +9,7 @@ export default class PublicCommitment extends Component {
     super(props)
     this.state = {
       weeklyDWTime: 0,
+      visibilityClassName: "visible"
     }
     this.props = {
     }
@@ -17,6 +18,7 @@ export default class PublicCommitment extends Component {
 
   componentDidMount() {
     this.getCommitments()
+    this.handleTimeVisibility()
   }
 
   getCommitments() {
@@ -45,35 +47,56 @@ export default class PublicCommitment extends Component {
       weeklyDWTime: weeklyDWTime
     })
   }
+
+  handleTimeVisibility() {
+    setInterval(()=>{
+      if(this.state.visibilityClassName == "visible") {
+        this.setState({
+          // visible:  false,
+          visibilityClassName: "invisible"
+        })
+        console.log(this.state.visibilityClassName)
+      } else {
+        this.setState({
+          // visible: true,
+          visibilityClassName: "visible"
+        })
+      }
+    }, 500)
+  }
+
   
   render() {
     return (
       <div>
         <div className="weeklyDWtime">
-          I'm focusing for <span className="DWtime">{this.state.weeklyDWTime}</span> hours this week.
+          Weekly focusing time <span className={`DWtime ${this.state.visibilityClassName}`}>{this.state.weeklyDWTime}</span> / 10:00 hours.
         </div> 
         <div className="desc">
           <div className="desc_title">What do I count?</div>  
-            <div className="desc_item">: Deep Work Time for 10 hours per week.</div>
+            <div className="desc_item">: The Deep Work time.</div>
           <div className="what_deep_work">
             <div className="desc_title">What is Deep Work Time?</div>  
-              <div className="desc_item">: ONLY in work in the Deep Work Time (*1) in Deep Work Days (*2).</div>
-              <div className="desc_item">: What is PLANED the day before.</div> 
-              <div className="desc_item">: What is done within VALID System or Process.</div>  
+              <div className="desc_item">: Only work in the Deep Work time (*1) in Deep Work days (*2).</div>
+              <div className="desc_item">: Only what is PLANED the day before.</div> 
+              <div className="desc_item">: Only what is done within VALID System or Process.</div>  
               </div> 
               <div className="annotation">
                 <div className="desc_item">*1 (Deep Work Time)</div>
-                <div className="desc_item">= 3-4 hours a day when I can focus at the DEEPEST level.</div> 
-                <div className="desc_item">= ONLY Tasks done in a place ONLY for Deep Work (Deep Work Space).</div>
-                <div className="desc_item">= ONLY AFTER Deep Work Ritual (Cold Shower and Meditation).</div>
+                <div className="desc_item">= Only 3-4 hours a day when I can focus at the DEEPEST level.</div> 
+                <div className="desc_item">= Only 3-4 hours a day when I can focus at the DEEPEST level.</div> 
+                <div className="desc_item">= Only Tasks done in a place ONLY for Deep Work (Deep Work Space).</div>
+                <div className="desc_item">= Only AFTER Deep Work Ritual (Cold Shower and Meditation).</div>
                 <div className="desc_item">*2 (Deep Work Days)</div>
-                <div className="desc_item">= The day I can focus at the DEEPEST level.</div> 
-                <div className="desc_item">= 4-5 Days a week.</div>
+                <div className="desc_item">= Only the day I can focus at the DEEPEST level.</div> 
+                <div className="desc_item">= Only 4-5 Days a week.</div>
               </div>
           </div>
           <div className="why">
+            <div className="desc_title">Why Deep Work?</div>  
+            <div className="desc_item">: I think it is the BEST way for the MAXIMUM outcome.</div>
             <div className="desc_title">Why do I publich my record?</div>  
-            <div className="desc_item">: To force myself to COMMIT to what I NEED to do by trying to evade from DEGRADING my PRIDE to YOU when I COULD NOT follow to it.   
+            <div className="desc_item">: To reinforce my commitment to what I need to do by trying to evade feel of shame if I could not follow to it.   
             </div>
           </div>      
 
