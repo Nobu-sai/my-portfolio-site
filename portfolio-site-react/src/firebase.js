6241d09aae35ef758be8c97b792df6d75ead90ef
snz0901@gmail.com <ngbt0409>
2021-03-04 00:15:48 +0900

    CHNG: Cloning from GitHub

--- portfolio-site-react/src/firebase.js
+++ portfolio-site-react/src/firebase.js
@@ -10,11 +10,12 @@ const firebaseDefaultProjectConfig  = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID 
 }
-
+// 
+//
 const firebaseCommitmentRecordProjectConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_COMMITMENT_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_COMMITMENT_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_COMMITMENT_DATABASE_URL,
+  // databaseURL: process.env.REACT_APP_FIREBASE_COMMITMENT_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_COMMITMENT_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_COMMITMENT_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_COMMITMENT_SENDER_ID,
